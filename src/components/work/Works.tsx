2b6b287437953f03c01540f20dbb3a137417bc13
GabrielGladykowski <gabriel.gladki2003@gmail.com>
2023-12-09 23:38:49 +0100

    Fix(Work section): creating a function that adds links

--- src/components/work/Works.tsx
+++ src/components/work/Works.tsx
@@ -5,6 +5,7 @@ import {
   WorkImage,
   WorkInProgress,
   WorkLink,
+  WorkLinkDescription,
   WorkStyled,
   WorkSubtitle,
   WorkTextWrapper,
@@ -72,37 +73,61 @@ export const Works: React.FC = () => {
 
   return (
     <WorkStyled id="work">
-      {GET_WORKS.allWorkJson.edges.map((element) => (
-        <WorkBorder key={element?.node.title} inProgress={element?.node.inProgress}>
-          <WorkWrapper>
-            <WorkInProgress inProgress={element?.node.inProgress}>
-              {element?.node.inProgressText}
-            </WorkInProgress>
-            <WorkTextWrapper inProgress={element?.node.inProgress}>
-              <WorkTitleWrapper>
-                <WorkTitle>{element?.node.title}</WorkTitle>
-                <WorkSubtitle>{element?.node.subtitle}</WorkSubtitle>
-              </WorkTitleWrapper>
-              <WorkDescription>{element?.node.description}</WorkDescription>
-            </WorkTextWrapper>
-            <WorkImage inProgress={element?.node.inProgress}>
-              <Img
-                style={{ width: '100%' }}
-                fluid={element?.node.image.childImageSharp.fluid}
-                alt={element?.node.imageDescription}
-              />
-            </WorkImage>
-            <WorkLink
-              inProgress={element?.node.inProgress}
-              href={element?.node.href}
-              target="_blank"
-              rel="noopener"
-            >
-              View Project
-            </WorkLink>
-          </WorkWrapper>
-        </WorkBorder>
-      ))}
+      {GET_WORKS.allWorkJson.edges.map((element) => {
+        const getParagraphFromString = (word: string): React.ReactNode => {
+          const arrayWithWordParts = word.split('|');
+
+          return arrayWithWordParts[1] ? (
+            <>
+              {arrayWithWordParts[0]}
+              <WorkLinkDescription
+                inProgress={element?.node.inProgress}
+                target="_blank"
+                rel="noopener"
+                href={arrayWithWordParts[1]}
+              >
+                {arrayWithWordParts[2]}
+              </WorkLinkDescription>
+            </>
+          ) : (
+            <>{arrayWithWordParts[0]}</>
+          );
+        };
+
+        return (
+          <WorkBorder key={element?.node.title} inProgress={element?.node.inProgress}>
+            <WorkWrapper>
+              <WorkInProgress inProgress={element?.node.inProgress}>
+                {element?.node.inProgressText}
+              </WorkInProgress>
+              <WorkTextWrapper inProgress={element?.node.inProgress}>
+                <WorkTitleWrapper>
+                  <WorkTitle>{element?.node.title}</WorkTitle>
+                  <WorkSubtitle>{element?.node.subtitle}</WorkSubtitle>
+                </WorkTitleWrapper>
+                <WorkDescription>
+                  {getParagraphFromString(element?.node.description)}
+                </WorkDescription>
+              </WorkTextWrapper>
+              <WorkImage inProgress={element?.node.inProgress}>
+                <Img
+                  style={{ width: '100%' }}
+                  fluid={element?.node.image.childImageSharp.fluid}
+                  alt={element?.node.imageDescription}
+                />
+              </WorkImage>
+              <WorkLink
+                inProgress={element?.node.inProgress}
+                href={element?.node.href}
+                target="_blank"
+                rel="noopener"
+              >
+                View Project
+              </WorkLink>
+            </WorkWrapper>
+          </WorkBorder>
+        );
+      })}
     </WorkStyled>
   );
 };
